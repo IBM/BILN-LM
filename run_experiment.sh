8036a24052d255d096730f8f436b9817f79da359
RaulFD-creator <raulfdz9@gmail.com>
2024-10-01 13:45:47 +0100

    🧹 Tidy code: Experiments with new local path

--- run_experiment.sh
+++ run_experiment.sh
@@ -7,14 +7,14 @@ python BILN-LM-OSS/code/download_data.py data downstream
 for dataset in c-binding c-cpp c-sol nc-binding nc-cpp; do
     for model in svm lightgbm; do
         for rep in ecfp molformer bilnlm pepclm pepland; do
-            python BILN-LM-OSS/code/represent_peptides.py $dataset $rep
+            python BILN-LM/code/represent_peptides.py $dataset $rep
             for fp in mapc ecfp; do
                 if [[ $fp == "mapc" ]]; then
                     sim=jaccard
                 else
                     sim=tanimoto
                 fi
-                    python BILN-LM-OSS/code/evaluation.py $dataset $model $sim $fp $rep
+                    python BILN-LM/code/evaluation.py $dataset $model $sim $fp $rep
                 done
             done
         done
