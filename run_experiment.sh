a603d3e8db65b61fca6783c9ef40b22f51bed49f
RaulFD-creator <raulfdz9@gmail.com>
2024-10-01 13:32:17 +0100

    🛠️ Code-dev: Extensive experiments

--- run_experiment.sh
+++ run_experiment.sh
@@ -2,10 +2,19 @@ dataset=nc-binding
 model=svm
 rep=bilnlm
 
-python BILN-LM-OSS/code/represent_peptides.py $dataset $rep
-
-for fp in map4 maccs; do
-    for sim in tanimoto dice cosine rogot-goldberg sokal; do
-        python BILN-LM-OSS/code/evaluation.py $dataset $model $sim $fp $rep
+for model in svm lightgbm; do
+    for dataset in c-binding c-cpp c-sol nc-binding nc-cpp; do
+        for rep in ecfp molformer bilnlm pepclm pepland; do
+            python BILN-LM-OSS/code/represent_peptides.py $dataset $rep
+            for fp in mapc ecfp; do
+                if [[ $fp == "mapc" ]]; then
+                    sim=jaccard
+                else
+                    sim=tanimoto
+                fi
+                    python BILN-LM-OSS/code/evaluation.py $dataset $model $sim $fp $rep
+                done
+            done
+        done
     done
 done
